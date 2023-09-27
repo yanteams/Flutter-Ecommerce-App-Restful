b32dee52fedbf4c5cbf0a9292de1727fa0e5f2b9
YanCoder <khoant20@uef.edu.vn>
2023-09-28 01:21:08 +0700

    Fetch Json Demo

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 const mongoose = require("mongoose");
+const productRoute = require('./routes/products');
+
 const port = 3000
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connect')).catch((err) => console.log(err))
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use('/api/products', productRoute);
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
